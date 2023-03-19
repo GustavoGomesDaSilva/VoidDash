878c5bac68e2c2138fd7c83a7abb6cf68095e955
Gugz3ra <gustavo.gomes.lhp@hotmail.com>
2023-03-19 00:46:28 -0300

    pagina do painel feita o front

--- views/areaDeControlePrincipal.php
+++ views/areaDeControlePrincipal.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/styleHome.css">
+    <link rel="stylesheet" href="../styles/stylePainel.css">
 
+    <link rel="shortcut icon" href="../components/assets/LogoVoidTech.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"> <!-- importantdo a font que será usada no site -->
@@ -15,8 +16,40 @@
 </head>
 
 <body>
+    
     <main>
-        FSAGSGS
+        <article id="menuEsquerda">
+            <section id="topoMenuEsquerda">
+                <img src="../components/assets/imgExemploPerfil.png" alt="">
+                <p id="nomeUsuario" name="nomeUsuario">Marcos Oliveira</p>
+                <p id="nomeUsuario" name="nomeUsuario">Analista de Dados</p>
+                <div id="iconsTopEsquerda">
+                    <img src="../components/assets/sairIcon.png" alt="">
+                    <img src="../components/assets/configIcon.png" alt="">
+                </div>
+            </section>
+            <section id="opcoesMenu">
+                <ul>
+                    <li> <img src="../components/assets/graficIcon.png" alt=""> Relatório</li>
+                    <li> <img src="../components/assets/manutencaoIcon.png" alt=""> Manutenção</li>
+                    <li> <img src="../components/assets/multasIcon.png" alt=""> Multas</li>
+                    <li> <img src="../components/assets/motoIcon.png" alt=""> Motoristas</li>
+                    <li> <img src="../components/assets/mapaIcon.png" alt=""> Rotas</li>
+                </ul>
+            </section>
+        </article>
+        <div id="conteudoPrincipal">
+            <header id="topoPainel">
+                <img src="../components/assets/LogoVoidTech.png" alt="">
+            </header>
+            <article id="painlePrincipal">
+                <div id="txtCentral">
+                    <p>V<span class="txtRoxo">o</span><span class="txtLaranja">i</span>dDash<br>
+                    Nunca foi tão <span class="txtRoxo">fácil</span>
+                    controlar sua <span class="txtRoxo">frota</span></p>
+                </div>
+            </article>
+        </div>
     </main>
 </body>
 
