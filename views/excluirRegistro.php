8ee1c5dcd9e852d86fd1aa8b83e4cdab3ace7390
Leonardo Dias <leo-ds@live.com>
2023-06-16 19:23:58 -0300

    finalizado 2.0

--- views/excluirRegistro.php
+++ views/excluirRegistro.php
@@ -7,11 +7,12 @@ $idregistro = $_POST['idRegistro'];
 // var_dump($_POST);
 
 // Prepara e executa a consulta SQL para deletar o registro
+
 $sql = "DELETE FROM `registros` WHERE `idRegistro` = '$idregistro'";
 if ($conn->query($sql) === TRUE) {
     echo "Registro deletado com sucesso!";
 } else {
-    echo "Erro ao deletar o registro: " . $conn->error;
+    echo "Erro ao deletar o registro: Este registro tem um sinistro pendente " ;
 }
 
 ?>
